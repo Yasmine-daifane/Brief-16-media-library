cdd9dd9e9e86c2727c5c8a72168bb5b3ea340d17
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-03-25 17:58:48 +0000

    add

--- inserdata.php
+++ inserdata.php
@@ -1,26 +1,26 @@
 
 
 <?php
-// include "connect.php";
+include "connect.php";
 
 
-// $json_file = file_get_contents('Books.json');
-// $books_array = json_decode($json_file, true);
-// foreach ($books_array as $book) {
-//     $title = $book['title'];
-//     $author = $book['auteur'];
-//     $image = $book['imageLink'];
-//     $state = $book['state'];
-//     $publishing_date = $book['year'];
-//     $pages = $book['pages'];
-//     $type = $book['type'];
+$json_file = file_get_contents('Books.json');
+$books_array = json_decode($json_file, true);
+foreach ($books_array as $book) {
+    $title = $book['title'];
+    $author = $book['auteur'];
+    $image = $book['imageLink'];
+    $state = $book['state'];
+    $publishing_date = $book['year'];
+    $pages = $book['pages'];
+    $type = $book['type'];
 
-//     $sql = "INSERT INTO  `ouvrage` (`titre`, `nom_de_l_auteur`, `l_mage_de_couverture`, `l_etat`, `type`, `date_d_achat`, `la_date_d_édition`, `N_pages`) 
-//        VALUES ('$title', ' $author', '$image', '$state', '$type',NOW() , '$publishing_date','$pages')";
+    $sql = "INSERT INTO  `ouvrage` (`titre`, `nom_de_l_auteur`, `l_mage_de_couverture`, `l_etat`, `type`, `date_d_achat`, `la_date_d_édition`, `N_pages`) 
+       VALUES ('$title', ' $author', '$image', '$state', '$type',NOW() , '$publishing_date','$pages')";
 
-//     $conn->exec($sql);}
+    $conn->exec($sql);}
 
-// $conn = null;
+$conn = null;
 
 
 ?>
