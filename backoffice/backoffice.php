49d44efd6560cd661f76e90c4043e4bcbb0f92a6
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-03-26 11:58:28 +0000

    ADD by the best

--- backoffice/backoffice.php
+++ backoffice/backoffice.php
@@ -1,86 +1,67 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BEBLIOTECAIRE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BEBLIOTECAIRE</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
     crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="backoffic.css">
-    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="backoffic.css">
+  <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 </head>
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light ">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div>
-          <img class=" logo mt-0" src="image_bckg/logo.png" alt="logo">
-          <p  style="font-size: 13;">BOOK lovers</p>
-        </div>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <div class="d-flex justify-content-around">
-                    <a class="nav-link" href="#">HOME</a>
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div>
+        <img class=" logo mt-0" src="../image_bckg/logo.png" alt="logo">
+        <p style="font-size: 13;">BOOK lovers</p>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div class="d-flex justify-content-around">
+            <a class="nav-link" href="#">HOME</a>
 
-            </div>
           </div>
-        </div>
-      </nav>
+      </div>
+    </div>
+  </nav>
 
-    <header>
-        <h1 class="text-center  fs-2 justify-content-center mt-5">Welcome to Book Lovers Library!<br>
-            Our website is dedicated to serving book lovers like you <svg xmlns="http://www.w3.org/2000/svg" width="50"
-                height="50" viewBox="0 0 24 24" style="vertical-align: middle;
+  <header>
+    <h1 class="text-center  fs-2 justify-content-center mt-5">Welcome to Book Lovers Library!<br>
+      Our website is dedicated to serving book lovers like you <svg xmlns="http://www.w3.org/2000/svg" width="50"
+        height="50" viewBox="0 0 24 24" style="vertical-align: middle;
                 position: absolute;
                 top: 51%;
                 right: 20%;
                 color: #abd5f9;">
-                <path fill="currentColor"
-                    d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z" />
-            </svg></h1>
-
-
-
-            <div class="container">
-
-                <div class="row height d-flex justify-content-center align-items-center">
-
-                  <div class="col-md-8">
-
-                    <div class="search">
-                      <i class="fa fa-search"></i>
-                      <input type="text" class="form-control" placeholder="Have a question? Ask Now">
-                      <button class="btn btn-primary">Search</button>
-                    </div>
-                    
-                  </div>
-                  
-                </div>
-            </div>
-
-        <!-- <iconify-icon icon="mdi:heart" style="color: #abd5f9;height: 10px; width: 10px;"></iconify-icon> -->
-    </header>
-
-
-
+        <path fill="currentColor"
+          d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z" />
+      </svg></h1>
+    <div class="container">
+      <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-8">
+          <div class="search">
+            <i class="fa fa-search"></i>
+            <input type="text" class="form-control" placeholder="Have a question? Ask Now">
+            <button class="btn btn-primary">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <iconify-icon icon="mdi:heart" style="color: #abd5f9;height: 10px; width: 10px;"></iconify-icon> -->
+  </header>
 </body>
-        <main>
-           
 
-
-
-
-
-        </main> 
-    
 </html>
