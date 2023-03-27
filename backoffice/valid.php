e73502ab41984edd3e7e5859f412daacd58c2724
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-03-27 02:03:41 +0000

    the last touch by the best of all time

--- backoffice/valid.php
+++ backoffice/valid.php
@@ -5,7 +5,7 @@ $id_member = $_POST['valid_member'];
 $id_book = $_POST['valid_book'];
 // $delete_reservation = "DELETE FROM reservation WHERE Id_reservation = '$id_reservation'";
 // $deleted = $conn->query($delete_reservation);
-$add_loan = "INSERT INTO `l_emprunt` (`Id_l_emprunt`, `la_date_d_emprunt`, `la_date_du_retour`, `Id_adhérent`, `Id_reservation`, `Id_ouvrage`) VALUES (NULL, NOW(), NULL, '$id_member', '$id_reservation', '$id_book')";
+$add_loan = "INSERT INTO `l_emprunt` (`Id_l_emprunt`, `la_date_d_emprunt`, `la_date_du_retour`, `Id_adhérent`, `Id_reservation`, `Id_ouvrage`) VALUES (NULL, NOW(), '', '$id_member', '$id_reservation', '$id_book')";
 $loaned = $conn->query($add_loan);
 header('Location: Tables.php');
 ?>
